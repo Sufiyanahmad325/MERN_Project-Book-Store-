8b8ffa556005b14544b9c1f75ad00e0d0433893b
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2024-07-12 07:37:15 +0530

    navbar below the nav bar there is right and left side is completed

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './componets/Navbar'
+import Banner from './componets/Banner'
+import Footer from './componets/Footer'
+import FreeBook from './componets/FreeBook'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar/>
+      <Banner/>
+      <FreeBook/>
+    </>
+  )
+}
+
+export default App
