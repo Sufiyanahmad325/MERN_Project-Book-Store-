d221a7b22d3aa9e3acf906e1b28dcdff27758d14
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2024-07-14 10:33:41 +0530

    backend setup completed

---
+++ Backend/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+dotenv.config({ path: "./.env" });
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_DB_URL = process.env.MONGODBURI
+
+
+
+
+
+
+//connected to mongodb
+const dbConnection = async() => {
+  await mongoose.connect(MONGO_DB_URL).then(() => {
+        console.log('Connected to MongoDB');
+    }).catch((error) => {
+        console.error('Error connecting to MongoDB:', error.message);
+    });
+
+}
+
+
+
+dbConnection().then(()=>{
+    app.listen(PORT, () => {
+        console.log(`Server listening on Port http://localhost:${PORT}`);
+    });
+})
