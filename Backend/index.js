96697036e8c132b4da81553f8ce5ac884b877a41
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2024-07-14 10:41:46 +0530

    controller is ready to use with router

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose';
+import router from './routes/book.route.js';
 dotenv.config({ path: "./.env" });
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_DB_URL = process.env.MONGODBURI
 
+app.use("/book", router)
 
 
 
@@ -15,7 +17,7 @@ const MONGO_DB_URL = process.env.MONGODBURI
 
 //connected to mongodb
 const dbConnection = async() => {
-  await mongoose.connect(MONGO_DB_URL).then(() => {
+  await  mongoose.connect(MONGO_DB_URL).then(() => {
         console.log('Connected to MongoDB');
     }).catch((error) => {
         console.error('Error connecting to MongoDB:', error.message);
