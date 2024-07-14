61d69e73cfaf5fc65f77abcbd3e45e5a32fabdc1
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2024-07-14 12:44:41 +0530

    user model ready

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,3 +1,29 @@
+import User from "../model/user.model"
 
 
 
+export const signup =async(req,res)=>{
+    const {fullname , email , password} = req.body
+
+    if([fullname , email , password].some(field=> field.trim() == "")){
+       return res.status(400).json({
+            success:false,
+            message:"Every field are required"
+        })
+    }
+
+
+    const existUser =await User.findOne({
+        email,
+    })
+
+
+
+    if(existUser){
+        return res.status(400).json({
+            success:true,
+            message:false
+        })
+    }
+
+}
